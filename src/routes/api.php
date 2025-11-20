208ff9208f197bfebaa515f3c3392a01e038f437
kigathi-chege <chegekigathi@gmail.com>
2025-11-20 09:33:14 +0300

    feat: add api middleware

--- src/routes/api.php
+++ src/routes/api.php
@@ -3,13 +3,17 @@
 use Lyre\Billing\Http\Controllers;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResources([
-    'subscriptions' => Controllers\SubscriptionController::class,
-    'subscriptionplans' => Controllers\SubscriptionPlanController::class,
-    'paymentmethods' => Controllers\PaymentMethodController::class,
-]);
+Route::prefix('api')
+    ->middleware(['api', \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class])
+    ->group(function () {
+        Route::apiResources([
+            'subscriptions' => Controllers\SubscriptionController::class,
+            'subscriptionplans' => Controllers\SubscriptionPlanController::class,
+            'paymentmethods' => Controllers\PaymentMethodController::class,
+        ]);
 
-Route::get('/subscriptionplans/{plan}/subscribe/', [Controllers\SubscriptionPlanController::class, 'subscribe']);
-Route::get('/subscriptions/{subscription}/approved/', [Controllers\SubscriptionController::class, 'approved']);
+        Route::get('/subscriptionplans/{plan}/subscribe/', [Controllers\SubscriptionPlanController::class, 'subscribe']);
+        Route::get('/subscriptions/{subscription}/approved/', [Controllers\SubscriptionController::class, 'approved']);
 
-Route::post('billing/webhook', Controllers\WebhookController::class)->name('webhook');
+        Route::post('billing/webhook', Controllers\WebhookController::class)->name('webhook');
+    });
